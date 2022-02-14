7aca45ba8ad56bedd4c02b2a73d39252ef3be246
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2022-02-14 16:58:50 +0100

    Creato colonne

---
+++ database/migrations/2022_02_14_153220_create_travels_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->string('destination', 15);
+            $table->date('departure');
+            $table->tinyInteger('hotel')->unsigned()->default(0);
+            $table->string('vehicle', 20);
+            $table->decimal('price', 7, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
